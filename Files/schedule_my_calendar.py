83ac148d6d5136b7af458c962f05833d69becf6e
Ioannis E. Kommas <54149422+johnkommas@users.noreply.github.com>
2023-10-11 12:18:36 +0300

    "Add new function for scheduled activities in calendar
    Introduced a new function called 'create_scheduled_activities' in 'schedule_my_calendar.py'. This function holds all the scheduled activities with their respective day of execution. This improves modularity, making it easier to add or modify scheduled activities, without affecting other parts of the code. Updated labels to uppercase for clarity."

---
+++ Files/schedule_my_calendar.py
@@ -0,0 +1,16 @@
+def create_scheduled_activities():
+    # Define your scheduled activities in this function
+    month_ = {
+        "4. Send Transactions: Accounting Office": {"day": 4, "month": None},
+        "5. DEH Self Metering": {"day": 5, "month": None},
+        "9. PROTERGIA Self Metering": {"day": 9, "month": None},  # None means every month
+        "12. Pay Day (VODAFONE HOME)": {"day": 12, "month": None},
+        "13. Pay Day (My DEH)": {"day": 13, "month": None},
+        "14. Pay Day (COSMOTE HOME)": {"day": 14, "month": None},
+        "15. Pay Day (PROTERGIA)": {"day": 15, "month": None},
+        "17. Pay Day (COSMOTE MOBILE)": {"day": 17, "month": None},
+        "21. Pay Day (VODAFONE MOBILE)": {"day": 21, "month": None},
+        "25. Create Special Prices": {"day": 25, "month": None},  # None means every month
+    }
+    scheduled_activities = month_
+    return scheduled_activities
