199b04327ba21d4a5684c4876858a6a356f1416d
Ioannis E. Kommas <54149422+johnkommas@users.noreply.github.com>
2023-11-28 11:57:57 +0200

    "Refactor fetch_data.py for readability and typo fixes
    The structure of the function get_sql_data in fetch_data.py is refactored for readability. It adds better indentation and proper spacing. The change also includes replacing single quotes with double quotes for consistency. A typo in the comment section is corrected for better understanding of the code. The structure changes will make future code maintenance easier."

--- SQL_FOLDER/fetch_data.py
+++ SQL_FOLDER/fetch_data.py
@@ -6,26 +6,34 @@ import pandas as pd
 import logging
 from Private import sql_connect
 
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', 1000)
-pd.set_option('display.max_rows', None)
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", 1000)
+pd.set_option("display.max_rows", None)
 
 logging.basicConfig(level=logging.INFO)
 
-def get_sql_data(sql_file: object, params: object = None, tuple_data: tuple = None, connection: object = sql_connect.connect()) -> object:
+
+def get_sql_data(
+    sql_file: object,
+    params: object = None,
+    tuple_data: tuple = None,
+    connection: object = sql_connect.connect(),
+) -> object:
     """
-        Executes a SQL query and returns the result as a pandas DataFrame.
-        The query is read from a file and optional bind parameters can be provided.
-    
-        :param sql_file: The file name (including its path) that contains the SQL query.
-        :param connection: The connection object to the SQL database.
-        :param params: An optional dictionary to be sent to the SQL query with bind parameters.
-                       Default is None, which means no parameters will be provided to the query.
-        :return: A pandas DataFrame with the results obtained from the SQL query.
-                 Returns None if an error occurred or no query was executed.
-        """
+    Executes a SQL query and returns the result as a pandas DataFrame.
+    The query is read from a file and optional bind parameters can be provided.
+
+    :param tuple_data:
+    :param sql_file: The file name (including its path) that contains the SQL query.
+    :param connection: The connection object to the SQL database.
+    :param params: An optional dictionary to be sent to the SQL query with bind parameters.
+                   Default is None, which means no parameters will be provided to the query.
+    :return: A pandas DataFrame with the results obtained from the SQL query.
+             Returns None if an error occurred or no query was executed.
+    """
     if connection == "2":
         connection = sql_connect.connect_lato()
+
     def get_query_from_file(sfile):
         script_directory = os.path.dirname(os.path.abspath(__file__))
         try:
@@ -53,5 +61,3 @@ def get_sql_data(sql_file: object, params: object = None, tuple_data: tuple = No
             logging.exception("Error occurred while executing SQL query: %s", e)
 
     return None
-
-
