8906583bf82b29716929662986e2efd6787c3e3c
johnkommas <johnkommas@gmail.com>
2025-02-15 20:52:47 +0200

    Refactor PoS text rendering and image positioning logic
    Simplified text labels for PoS A and B by removing redundant prefixes. Adjusted text rendering coordinates and spacing, and updated image positioning offsets for better alignment.

--- Entersoft/PoS.py
+++ Entersoft/PoS.py
@@ -17,12 +17,12 @@ def get_Pos(path, images, editables, font):
 
     # Στατιστικά για κάθε PoS
     # df_pos_a = df[df["POSID"] == "???????"]
-    data_to_text_pos_a = "POS A: Επ:000 Απ:00"
+    data_to_text_pos_a = "Επ:000 Απ:00"
     # for status, count in zip(df_pos_a.Status.value_counts().index, df_pos_a.Status.value_counts()):
     #     data_to_text_pos_b = data_to_text_pos_b + f"{count} "
 
     df_pos_b = df[df["POSID"] == "01655516"]
-    data_to_text_pos_b = "POS B: "
+    data_to_text_pos_b = ""
     for status, count in zip(df_pos_b.Status.value_counts().index, df_pos_b.Status.value_counts()):
         data_to_text_pos_b = data_to_text_pos_b + f"{status[:2]}:{count} "
 
@@ -32,18 +32,18 @@ def get_Pos(path, images, editables, font):
         text_offset = 0
         for entry in data_to_text:
             editable.text(
-                (3400, 80 + text_offset), entry, os.getenv("COLOR_A"), font=font
+                (2050 + text_offset, 160), entry, os.getenv("COLOR_A"), font=font
             )  # Χρήση καθεμιάς εγγραφής
-            text_offset += 100
+            text_offset += 1560
 
     # Λειτουργία για την κατασκευή της εικόνας
     def handle_image(pos_condition, offset, image):
         path_a = f"{path}/bad_pos.png" if pos_condition else f"{path}/good_pos.png"
-        box_ = (2800 + offset, 50)
+        box_ = (1550 + offset, 0)
         image = minimalist_write.paste_image(image, path_a, box_, resize=2)
 
     for image in images:
         # Εικόνα για pos_a
         handle_image(pos_a, 0, image)
         # Εικόνα για pos_b
-        handle_image(pos_b, 250, image)
+        handle_image(pos_b, 1550, image)
