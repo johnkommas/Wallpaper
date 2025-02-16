ee81456c1bc52b0705ba224241ef3ebb6ad8dbd8
johnkommas <johnkommas@gmail.com>
2025-02-16 16:50:11 +0200

    Add PDA SQL query and integrate PDA plotting functionality
    Introduced a new SQL query to analyze PDA-related data and added the PDA plotting function to the Python script. This enhances data visualization with a Sankey diagram for PDA statistics. Updated dependencies to include the new PDA module functionality.

---
+++ SQL_FOLDER/PDA.sql
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) Ioannis E. Kommas 2023. All Rights Reserved
+ */
+
+SELECT
+    COUNT(DISTINCT IMP_MobileDocumentHeaders.Code) AS DOCS,
+    COUNT(IMP_MobileDocumentLines.BarCode) AS LINES,
+    IMP_MobileDocumentHeaders.PdaId AS PdaId,
+    CASE
+        WHEN OrderType IN ('ΑΤΔ', 'ΑΔΠ', 'ΑΧΔ') THEN 'ΑΓΟΡΕΣ'
+        WHEN OrderType = 'ΔΕΑ' THEN 'ΕΠΙΣΤΡΟΦΕΣ'
+        WHEN OrderType = 'ΔΕΝ' THEN 'ΕΝΔΟΔΙΑΚΙΝΗΣΕΙΣ'
+        WHEN OrderType = 'ΠΠΡ' THEN 'ΠΑΡΑΓΓΕΛΙΕΣ'
+        WHEN OrderType = 'ΑΠ_ΜΟΒ' THEN 'ΡΑΦΙ ΤΙΜΕΣ'
+        ELSE 'ΛΟΙΠΑ'
+        END AS TYPE,
+    CASE
+        WHEN IMP_MobileDocumentHeaders.CheckState = 1 THEN 'Ναι'
+        ELSE 'Όχι'
+        END AS Katahorimeno,
+    FORMAT(RealImportTime, 'MMM', 'el-GR') AS 'MONTH'
+FROM IMP_MobileDocumentLines
+         LEFT JOIN IMP_MobileDocumentHeaders
+                   ON IMP_MobileDocumentHeaders.GID = IMP_MobileDocumentLines.fDocGID
+         LEFT JOIN ESFITradeAccount
+                   ON ESFITradeAccount.gid = IMP_MobileDocumentHeaders.Supplier
+WHERE
+    DATEPART(YEAR, RealImportTime) = DATEPART(YEAR, GETDATE())
+GROUP BY
+    CASE
+        WHEN OrderType IN ('ΑΤΔ', 'ΑΔΠ', 'ΑΧΔ') THEN 'ΑΓΟΡΕΣ'
+        WHEN OrderType = 'ΔΕΑ' THEN 'ΕΠΙΣΤΡΟΦΕΣ'
+        WHEN OrderType = 'ΔΕΝ' THEN 'ΕΝΔΟΔΙΑΚΙΝΗΣΕΙΣ'
+        WHEN OrderType = 'ΠΠΡ' THEN 'ΠΑΡΑΓΓΕΛΙΕΣ'
+        WHEN OrderType = 'ΑΠ_ΜΟΒ' THEN 'ΡΑΦΙ ΤΙΜΕΣ'
+        ELSE 'ΛΟΙΠΑ'
+        END,
+    IMP_MobileDocumentHeaders.CheckState,
+    FORMAT(RealImportTime, 'MMM', 'el-GR'), IMP_MobileDocumentHeaders.PdaId;
