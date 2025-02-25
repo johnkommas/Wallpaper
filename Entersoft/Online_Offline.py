985e863a9090e58d0218cb830b1fb31ac97677a0
johnkommas <johnkommas@gmail.com>
2025-02-25 22:20:32 +0200

    Update position calibration logic in online_offline function
    Revised position calculations by introducing `calibrate_y_2` for better alignment. Adjusted coordinates in `potitions` and `image_potitions` lists to use the new calibration variable. This improves overall layout accuracy.

--- Entersoft/Online_Offline.py
+++ Entersoft/Online_Offline.py
@@ -3,26 +3,26 @@ from Files import minimalist_write
 
 
 def online_offline(images, editables, status_users_elounda, path, timestamp_font_parse, i=0, path_b=None,):
-    calibrate_y = 280
-    calibrate_x = 300
+    calibrate_y = 500
+    calibrate_y_2 = calibrate_y + 610
     potitions = [
-        (8500, 4100 - calibrate_y),  # GIOTA 5Days
-        (7500, 3680 - calibrate_y),  # KOUTOULAKI 9h.29m
+        (7500, 4100 - calibrate_y_2),  # GIOTA 5Days
+        (8500, 3680 - calibrate_y_2),  # KOUTOULAKI 9h.29m
         (8500, 2250 - calibrate_y),  # KYRIAKOS 4h.1m
         (8500, 1400 - calibrate_y),  # M.KOUTOULAKIS 12h.52m
         (7500, 1800 - calibrate_y),  # M.RAPANAKI 3h.52m
         (7500, 950 - calibrate_y),  # RADMIN 13h.57m
-        (8500, 3250 - calibrate_y),  # XNARAKI 3h.58m
+        (7500, 3250 - calibrate_y_2),  # XNARAKI 3h.58m
     ]
 
     image_potitions = [
-        (8080, 3980 - calibrate_y),  # GIOTA
-        (8230, 3550 - calibrate_y),  # KOUTOULAKI
+        (8230, 3980 - calibrate_y_2),  # GIOTA
+        (8080, 3550 - calibrate_y_2),  # KOUTOULAKI
         (8080, 2100 - calibrate_y),  # KYRIAKOS
         (8080, 1250 - calibrate_y),  # M.KOUTOULAKIS
         (8230, 1670 - calibrate_y),  # M.RAPANAKI
         (8230, 830 - calibrate_y),  # RADMIN
-        (8080, 3130 - calibrate_y),  # XNARAKI
+        (8230, 3130 - calibrate_y_2),  # XNARAKI
     ]
     EM_Users = os.getenv("EMUSERS").split(",")
     for image, editable in zip(images, editables):
