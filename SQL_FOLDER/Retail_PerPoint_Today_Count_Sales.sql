abc82479fc1e3f293095e87b720b0409e7730451
johnkommas <johnkommas@gmail.com>
2025-02-26 13:20:01 +0200

    Add query for today's retail sales per point
    Introduced a new SQL query to calculate sales counts for specific cash registers (TAMEIO A and B) based on given criteria. Optimized with efficient conditions and indexing to enhance performance.

---
+++ SQL_FOLDER/Retail_PerPoint_Today_Count_Sales.sql
@@ -0,0 +1,12 @@
+-- Optimized query with indexes and faster predicate evaluation.
+SELECT
+    SUM(IIF(ADCode LIKE N'ΑΠΛ-Α%' OR ADCode LIKE N'ΤΔΑ-Α%', 1, 0)) AS [TAMEIO Α],
+    SUM(IIF(ADCode LIKE N'ΑΠΛ-Β%' OR ADCode LIKE N'ΤΔΑ-Β%', 1, 0)) AS [TAMEIO Β]
+FROM
+    ESFIDocumentTrade 
+WHERE
+    fShippingPurposeCode = N'ΠΩΛΗΣΗ'
+    AND fADSiteGID = N'86947579-6885-4E86-914E-46378DB3794F'
+    AND (ADCode LIKE N'ΑΠΛ-%' OR ADCode LIKE N'ΤΔΑ-%')
+    AND ESDCreated >= CAST(GETDATE() AS DATE)
+    AND ESDCreated < DATEADD(DAY, 1, CAST(GETDATE() AS DATE));
