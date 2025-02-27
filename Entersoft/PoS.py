887d775c2bf2f0881f8bca42fd42420df8ad60e2
johnkommas <johnkommas@gmail.com>
2025-02-27 17:18:27 +0200

    Add polar chart generation for card payment visualization
    Introduced a function to generate polar charts displaying the ratio of card payments to receipts for POS devices. Updated existing image handling logic to include these charts, leveraging matplotlib and numpy. This enhances visual representation of payment data.

--- Entersoft/PoS.py
+++ Entersoft/PoS.py
@@ -1,3 +1,6 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
 from SQL_FOLDER import fetch_data
 import os
 from Files import minimalist_write
@@ -19,6 +22,10 @@ def get_Pos(path, images, editables, font):
     # Έλεγχος των συνθηκών για pos_a και pos_b
     pos_a = df.loc[df["POSID"] == PDA_ID_A, "Status"].eq("Εκκρεμής").any()
     pos_b = df.loc[df["POSID"] == PDA_ID_B, "Status"].eq("Εκκρεμής").any()
+
+    Card_Payments_A = df[(df["POSID"] == PDA_ID_A) & (df["Status"] == "Επιτυχημένη")].shape[0]
+    Card_Payments_B = df[(df["POSID"] == PDA_ID_B) & (df["Status"] == "Επιτυχημένη")].shape[0]
+    Card_Payments = [Card_Payments_A, Card_Payments_B]
 
     if pos_a or pos_b:
         sound.get_notified()
@@ -45,13 +52,90 @@ def get_Pos(path, images, editables, font):
             text_offset += 1540
 
     # Λειτουργία για την κατασκευή της εικόνας
-    def handle_image(pos_condition, offset, image):
+    def handle_image(pos_condition, offset, image, my_retail):
         path_a = f"{path}/bad_pos.png" if pos_condition else f"{path}/good_pos.png"
         box_ = (1980 + offset, 1150)
+        box_polar = (1650 + offset, 1400)
         image = minimalist_write.paste_image(image, path_a, box_, resize=2)
+        image = minimalist_write.paste_image(image, my_retail, box_polar, resize=3)
+
+    for enum, (a, b) in enumerate(zip(Card_Payments, retail_per_point)):
+        polar_chart(a, b, f"{path}/polar_chart_{enum}.png")
 
     for image in images:
         # Εικόνα για pos_a
-        handle_image(pos_a, 0, image)
+        handle_image(pos_a, 0, image, f"{path}/polar_chart_0.png")
         # Εικόνα για pos_b
-        handle_image(pos_b, 1550, image)
+        handle_image(pos_b, 1550, image, f"{path}/polar_chart_1.png")
+
+
+def polar_chart(cards, receipts, file, title="ΠΟΣΟΣΤΟ ΣΥΝΑΛΛΑΓΩΝ ΜΕ ΚΑΡΤΑ"):
+    colors = [os.getenv("COLOR_A"), os.getenv("COLOR_B"), os.getenv("COLOR_C")]  # Green, Orange, Red
+    ranges = [50, 25, 25]  # Ranges for low, medium and high
+
+    fig = plt.figure(figsize=(8, 8), dpi=450)
+    ax = fig.add_subplot(projection="polar")
+
+    # Create bars for each category (low, medium, high), but now starting from right
+    start = 0
+    bar_widths = []
+    for i, (col, rng) in enumerate(
+            zip(colors[::-1], ranges[::-1])
+    ):  # Reversed colors and ranges
+        width = rng / 100 * np.pi  # Ranges are out of 100%
+        bar_widths.append(width)
+        ax.bar(
+            start,
+            height=1,
+            width=width,
+            bottom=2,
+            linewidth=3,
+            edgecolor="none",
+            color=col,
+            align="edge",
+        )
+        start += width
+
+    # Get the percentage, for  50% of the 'medium' range
+    if receipts == 0:  # Handle 0 receipts
+        percent = 0  # Or set it to a default value
+    else:
+        percent = cards / receipts
+
+    angle = np.pi - percent * np.pi
+    text = f"{round(100 * percent)}%"
+
+    # Determine arrow color
+    if angle < bar_widths[0]:
+        color = colors[2]
+    elif angle < sum(bar_widths[:2]):
+        color = colors[1]
+    else:
+        color = colors[0]
+
+    plt.annotate(
+        text,
+        xytext=(0, 0),
+        xy=(angle, 2.0),
+        arrowprops=dict(arrowstyle="wedge, tail_width=0.8", color=color, shrinkA=0),
+        bbox=dict(boxstyle="circle", facecolor=color, linewidth=1.0, edgecolor=color),
+        fontsize=40,
+        color=colors[-1],
+        ha="center",
+    )
+    # Add the title at the bottom of the chart
+    # plt.figtext(
+    #     0.5,  # X-coordinate of the title (centered at the middle)
+    #     0.01,  # Y-coordinate of the title (just above the bottom edge)
+    #     title,  # The title text to display
+    #     fontsize=12,  # Font size
+    #     color="black",  # Title text color
+    #     ha="center",  # Horizontal alignment ("center" aligns in the middle)
+    # )
+
+    ax.set_axis_off()
+    plt.savefig(file, transparent=True, bbox_inches="tight", dpi=450)
+    plt.clf()
+    plt.cla()
+    plt.close()
+
