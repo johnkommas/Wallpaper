781b19b7a1ab03be2b70fb462c4ba2228d0ae301
johnkommas <johnkommas@hotmail.com>
2025-06-14 22:37:09 +0300

    Add ANSI color codes for terminal output in standalone POS alert script

--- pos_alert_stand_alone.py
+++ pos_alert_stand_alone.py
@@ -4,29 +4,38 @@ from dotenv import load_dotenv
 import os
 import time
 
+# Ορισμός χρωμάτων ANSI για το τερματικό
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+RED = '\033[91m'
+RESET = '\033[0m'
+
 load_dotenv()
+
+
 
 def run():
     # Εκτέλεση SQL ερωτήματος και φόρτωση δεδομένων
-    query = ["Pos_Pending.sql"]
+    query = "Pos_Pending.sql"
 
-    df = fetch_data.get_sql_data(query[0])
+    df = fetch_data.get_sql_data(query)
     if df.empty:
         return
 
     imessage.mailme(df.iloc[0, 0])
 
 
+
 if __name__ == "__main__":
     refresh_rate = int(os.getenv("REFRESH_TIMER"))
-    while True:
+    while True: 
         try:
-            print(f"Job Started: {time.ctime()} || ", end='')
+            print(f"{GREEN}Job Started: {time.ctime()} {RESET}|| ", end='')
             startTime = time.monotonic()
             run()
             elapsed = time.monotonic() - startTime
-            print(f"Job Ended: Execution time: {round(elapsed, 2)}sec ||", end='')
-            print(f" Next Refresh @: {time.ctime(time.time() + refresh_rate)}")
+            print(f"{YELLOW}Job Ended: Execution time: {round(elapsed, 2)}sec {RESET}||", end='')
+            print(f" {RED} Next Refresh @: {time.ctime(time.time() + refresh_rate)}{RESET}")
             time.sleep(refresh_rate)
         except KeyboardInterrupt:
             break
